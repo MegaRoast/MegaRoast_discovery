3530303f58100d412b37a244de09540540ed114c
mihay42 <mihay42@gmail.com>
2024-12-28 10:01:08 -0800

    Incremental update

--- github-reports/src/components/CompanyIconView.js
+++ github-reports/src/components/CompanyIconView.js
@@ -1,18 +1,17 @@
 import React, { useState, useRef } from 'react';
 import { Box, Text } from '@primer/react';
 import Tooltip from '@mui/material/Tooltip';
-import styles from './CompanyIconView.module.css';
 import { useTheme } from '@mui/material/styles';
-import { PiBuilding, PiCertificate } from 'react-icons/pi';
-import { HiOutlineMap } from 'react-icons/hi2';
+import styles from './CompanyIconView.module.css';
+import tooltipStyles from './tooltipStyles.module.css';
+import { OrganizationIcon, GlobeIcon, BriefcaseIcon } from '@primer/octicons-react';
 
 const CompanyIconView = ({ companies }) => {
-  // eslint-disable-next-line no-unused-vars
+  const theme = useTheme();
+
   const [showTooltip, setShowTooltip] = useState(false);
-  // eslint-disable-next-line no-unused-vars
   const [tooltipContent, setTooltipContent] = useState({});
   const target = useRef(null);
-  const theme = useTheme();
 
   const handleMouseEnter = (company, event) => {
     setTooltipContent(company);
@@ -30,26 +29,26 @@ const CompanyIconView = ({ companies }) => {
         <Tooltip
           key={index}
           title={
-            <Box className={styles.tooltip}>
-              <Box className={styles.tooltipItem}>
-                <Text className={styles.tooltipHeading}>Company Name</Text>
-                <Box className={styles.tooltipContent}>
-                  <PiBuilding size={12} className={styles.tooltipIcon} />
-                  <Text className={styles.tooltipText}>{company.name}</Text>
+            <Box className={tooltipStyles.tooltip}>
+              <Box className={tooltipStyles.tooltipItem}>
+                <Text className={tooltipStyles.tooltipHeading}>Company Name</Text>
+                <Box className={tooltipStyles.tooltipContent}>
+                  <OrganizationIcon size={12} className={tooltipStyles.tooltipIcon} />
+                  <Text className={tooltipStyles.tooltipText}>{company.name}</Text>
                 </Box>
               </Box>
-              <Box className={styles.tooltipItem}>
-                <Text className={styles.tooltipHeading}>Region</Text>
-                <Box className={styles.tooltipContent}>
-                  <HiOutlineMap size={12} className={styles.tooltipIcon} />
-                  <Text className={styles.tooltipText}>{company.region}</Text>
+              <Box className={tooltipStyles.tooltipItem}>
+                <Text className={tooltipStyles.tooltipHeading}>Region</Text>
+                <Box className={tooltipStyles.tooltipContent}>
+                  <GlobeIcon size={12} className={tooltipStyles.tooltipIcon} />
+                  <Text className={tooltipStyles.tooltipText}>{company.region}</Text>
                 </Box>
               </Box>
-              <Box className={styles.tooltipItem}>
-                <Text className={styles.tooltipHeading}>Type</Text>
-                <Box className={styles.tooltipContent}>
-                  <PiCertificate size={12} className={styles.tooltipIcon} />
-                  <Text className={styles.tooltipText}>{company.company_type}</Text>
+              <Box className={tooltipStyles.tooltipItem}>
+                <Text className={tooltipStyles.tooltipHeading}>Type</Text>
+                <Box className={tooltipStyles.tooltipContent}>
+                  <BriefcaseIcon size={12} className={tooltipStyles.tooltipIcon} />
+                  <Text className={tooltipStyles.tooltipText}>{company.company_type}</Text>
                 </Box>
               </Box>
             </Box>
@@ -58,21 +57,15 @@ const CompanyIconView = ({ companies }) => {
           followCursor
           PopperProps={{
             sx: {
-                '& .MuiTooltip-tooltip': {
-                  backgroundColor: theme.palette.mode === 'dark' ? '#1e1e1e' : '#ffffff',
-                  color: theme.palette.mode === 'dark' ? '#ffffff' : '#000000',
-                  border: `1px solid ${theme.palette.primary.main}`,
-                  borderRadius: '4px',
-                  boxShadow: '0 4px 8px rgba(0, 0, 0, 0.1)',
-                  fontSize: '9px',
-                  padding: '8px',
-                  width: '200px',
-                  textAlign: 'left',
-                },
-                '& .MuiTooltip-arrow': {
-                  color: theme.palette.mode === 'dark' ? '#1e1e1e' : '#ffffff',
-                },
+              '& .MuiTooltip-tooltip': {
+                '--tooltip-background-color': theme.palette.mode === 'dark' ? '#1e1e1e' : '#ffffff',
+                '--tooltip-text-color': theme.palette.mode === 'dark' ? '#ffffff' : '#000000',
+                '--tooltip-border-color': theme.palette.primary.main,
               },
+              '& .MuiTooltip-arrow': {
+                '--tooltip-background-color': theme.palette.mode === 'dark' ? '#1e1e1e' : '#ffffff',
+              },
+            },
           }}
         >
           <Box
